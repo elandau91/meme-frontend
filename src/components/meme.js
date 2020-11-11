d7b24bac4cbce48d0d07335d019c81b82cbd8ecc
elandau91 <eli.b.landau@gmail.com>
2020-11-11 10:48:13 -0500

    hooks for text, just the start

--- src/components/meme.js
+++ src/components/meme.js
@@ -5,18 +5,27 @@ import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
 
 
+
 function Meme(props){
     const [show, setShow] = useState(false);
-  
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const [topY, setTopY] = useState("10%");
+    const [topX, setTopX] = useState("50%");
+    const [bottomY, setBottomY] = useState("90%");
+    const [bottomX, setBottomX] = useState("50%");
+    const [toptext, setTopText] = useState("");
+    const [bottomtext, setBottomText] = useState("");
+
+    //const topChangeHandler = (e) => setTopText({toptext: e.target.value})
+
     const base_image = new Image();
     base_image.src = props.meme.url;
     const wrh = base_image.width / base_image.height;
     const newWidth = 600;
     const newHeight = newWidth / wrh;
 
-
+    //console.log()
     return(
         <div className="meme_cards">
         <Card  style={{ width: '18rem' }}>
@@ -52,15 +61,36 @@ function Meme(props){
                 height={newHeight}
                 width={newWidth}
               />
+              <text
+                x={topX}
+                y={topY}
+                dominantBaseline="middle"
+                textAnchor="middle"
+                onMouseDown={event => this.handleMouseDown(event, 'top')}
+                onMouseUp={event => this.handleMouseUp(event, 'top')}
+              >
+                  {toptext}
+              </text>
+              <text
+                dominantBaseline="middle"
+                textAnchor="middle"
+                x={bottomX}
+                y={bottomY}
+                onMouseDown={event => this.handleMouseDown(event, 'bottom')}
+                onMouseUp={event => this.handleMouseUp(event, 'bottom')}
+              >
+                  {bottomtext}
+              </text>
             </svg>
+
               <div className="form">
                 <Form.Group>
                     <Form.Label>Top Text</Form.Label>
-                    <Form.Control type="text" placeholder="Type here"/>
+                    <Form.Control type="text" placeholder="Type here" onChange={(e) => setTopText(e.target.value)}/>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Bottom Text</Form.Label>
-                    <Form.Control type="text" placeholder="Type here"/>
+                    <Form.Control type="text" placeholder="Type here" onChange={(e) => setBottomText(e.target.value)}/>
                 </Form.Group>
               </div>
           </Modal.Body>
